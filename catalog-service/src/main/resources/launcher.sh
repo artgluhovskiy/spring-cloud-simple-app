1d77e583d14dde9f2532079a3138e9b7b74742c7
Artsiom Hlukhouski <gluhovskiy.art@gmail.com>
2018-12-13 16:58:51 +0300

    [Spring Cloud Simple App] Configuration properties - minor changes.

--- catalog-service/src/main/resources/launcher.sh
+++ catalog-service/src/main/resources/launcher.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 echo "Catalog Service is launching..."
-while ! (nc -z ${DATABASE_HOST} ${DATABASE_PORT} && nc -z ${CONFIG_SERVER_HOST} ${CONFIG_SERVER_PORT}); do
+while ! (nc -z ${DATABASE_HOST} ${DATABASE_PORT}); do
     echo "Trying to connect to MySQL at ${DATABASE_HOST}:${DATABASE_PORT}..."
-    echo "Trying to connect to Config Server at ${CONFIG_SERVER_HOST}:${CONFIG_SERVER_PORT}..."
-    sleep 60
+    sleep 10
 done
 echo ">> connected to MySQL database! <<"
+while ! (nc -z ${CONFIG_SERVER_HOST} ${CONFIG_SERVER_PORT}); do
+    echo "Trying to connect to Config Server at ${CONFIG_SERVER_HOST}:${CONFIG_SERVER_PORT}..."
+    sleep 10
+done
 echo ">> connected to Config Server! <<"
 java -Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8091 -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /app.jar
